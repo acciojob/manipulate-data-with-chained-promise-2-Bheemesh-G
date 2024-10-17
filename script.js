7700cc4e19c9b0cef82326ad58fd4669ec253061
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-17 16:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //your JS code here. If required.
+
+const arr = [1,2,3,4];
+
+function firstPromise(arr)
+{
+	return new Promise((resolve,reject)=>{
+		setTimeout((arr)=>{
+			resolve(arr);
+		},3000)
+	})
+}
+
+
+function printOddNumbers(arr)
+{
+	return new Promise((resolve,reject)=>{
+		setTimeout((arr)=>{
+			const oddArray = [];
+			for(let i=0;i<arr.length;i++)
+				{
+					if(arr[i]%2!==0)
+					{
+						oddArray.push(arr[i]);
+					}
+				}
+			resolve(oddArray);
+		},1000)
+	})
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	firstPromise(arr)
+	.then((firstPromiseArr)=>{
+		printOddNumbers(firstPromiseArr);
+	})
+	.then((printOddNumbersArr)={
+		document.getElementById('output').textContent = printOddNumbers;
+	})
+	.catch((error)=>{
+		console.log(error);
+	})
+
+	
+});
+
+
+
+
+
+
+
